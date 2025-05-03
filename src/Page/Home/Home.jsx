4b5e654a6d7c9869ee5a425b7109c9ac6f691019
Md. Seam <seam5559@gmail.com>
2025-05-03 16:43:41 +0600

    update

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import Hero from "./HomeComponents/Hero";
 import HomeCategory from "./HomeComponents/HomeCategory";
 import PopularCourse from "./HomeComponents/PopularCourse";
-// import bgImage from "../../assets/img/bgg.png";
-import bgImage2 from "../../assets/img/bg1.png";
-import SuccesHistory from "./HomeComponents/SuccesHistory";
 import WhatWeProvide from "./HomeComponents/WhatWeProvide";
 import Employe from "./HomeComponents/Employe";
 import Concerns from "./HomeComponents/Concerns";
@@ -15,16 +12,17 @@ import { MessageCircle } from "lucide-react";
 
 const Home = () => {
   return (
-    <div className=" ">
-      <div className="min-h-[calc(100vh-26rem)] py-12 md:py-16 lg:py-16 w-full relative bg-[#E6F2FF]">
+    <div className="bg-gray-50">
+      <div className="min-h-[calc(100vh-15rem)] py-12 md:py-16 lg:py-16 w-full relative container mx-auto flex items-center">
         <Hero />
       </div>
       <HomeCategory />
       <div className="bg-gray-50 py-16">
-        <div className="w-full sm:w-11/12 lg:w-11/12 2xl:w-12/12 mx-auto px-4 sm:px-6 lg:px-8 rounded-2xl bg-[#E6F2FF] shadow-xl">
+        <div className="bg-cover w-full sm:w-11/12 lg:w-11/12 2xl:w-12/12 mx-auto px-4 sm:px-6 lg:px-8 rounded-2xl bg-gray-50 shadow-xl">
           <PopularCourse />
         </div>
       </div>
+      
       <Concerns></Concerns>
       <SeminarAndEvent></SeminarAndEvent>
       {/* <SuccesHistory></SuccesHistory> */}
